520e46c843eb97c4f913e48bd7cb4e0408f38244
Adhikari <apabaad@yahoo.com>
2022-12-05 09:09:24 +1100

    outdoor page live

--- assets/theme.js
+++ assets/theme.js
@@ -8749,5 +8749,6 @@ for (let i = 0, linksLength = links.length ; i < linksLength ; i++) {
   if (links[i].hostname !== window.location.hostname) {
     links[i].target = '_blank';
     links[i].rel = 'noreferrer noopener';
+    $(links[i]).replaceWith($(links[i]).clone());
   }
 }
